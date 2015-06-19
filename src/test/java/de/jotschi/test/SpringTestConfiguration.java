1ce764841d000dc902b7f679711dfd564934c25b
Johannes Schüth <j.schueth@gentics.com>
2015-06-19 12:58:08 +0200

    Add missing annotation

--- src/test/java/de/jotschi/test/SpringTestConfiguration.java
+++ src/test/java/de/jotschi/test/SpringTestConfiguration.java
@@ -3,10 +3,12 @@ package de.jotschi.test;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.springframework.context.annotation.aspectj.EnableSpringConfigured;
 
 @Configuration
 @ComponentScan(basePackages = { "de.jotschi" })
 @EnableAspectJAutoProxy
+@EnableSpringConfigured
 public interface SpringTestConfiguration {
 
 }
